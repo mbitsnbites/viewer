9887350a1d7a5671aad10c8a3f28ee0b91f01818
m <m@bitsnbites.eu>
2016-02-27 10:23:02 +0100

    Make all them builds fail

--- src/viewer/main.cc
+++ src/viewer/main.cc
@@ -28,6 +28,10 @@
 
 #include <iostream>
 
+// HACK: This should make the Mac & Linux builds fail.
+// Even the Windows build should fail.
+#include <windows.h>
+
 #include "viewer/error.h"
 #include "viewer/viewer.h"
 
